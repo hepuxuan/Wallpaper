a1bdbab38f93a6364edc43e2153589d2675bb48b
Puxuan He <puxuanhe@gmail.com>
2018-07-13 16:01:22 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,101 @@
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu
+} = require('electron');
+const path = require('path');
+const addon = require('../build/Release/addon');
+const AutoLaunch = require('auto-launch');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow();
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/wp1/index.html`);
+
+  const appIcon = new Tray(path.join(__dirname, 'icon.png'));
+
+  const contextMenu = Menu.buildFromTemplate([
+     {
+      label: 'Quit',
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  appIcon.setContextMenu(contextMenu);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+  mainWindow.setFullScreen(true);
+  mainWindow.setMenuBarVisibility(false);
+  addon.setWallpaper(mainWindow.getNativeWindowHandle());
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+const autoLauncher = new AutoLaunch({
+  name: 'dynamicwallpaper',
+});
+
+autoLauncher.isEnabled()
+    .then((isEnabled) => {
+      if (arg) {
+        if (isEnabled) {
+          return;
+        }
+        console.log('enable');
+        autoLauncher.enable();
+      } else {
+        if (!isEnabled) {
+          return;
+        }
+        console.log('disable');
+        autoLauncher.disable();
+      }
+    }).catch(() => {});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ wallpaper.cpp
@@ -0,0 +1,67 @@
+#include <node.h>
+#include <node_buffer.h>
+#include <Windows.h>
+#include <stdio.h>
+
+HWND _workerw = nullptr;
+
+BOOL CALLBACK EnumWindowsProc(_In_ HWND tophandle, _In_ LPARAM topparamhandle)
+{
+  HWND defview = FindWindowEx(tophandle, NULL, "SHELLDLL_DefView", NULL);
+  if (defview != nullptr)
+  {
+    _workerw = FindWindowEx(NULL, tophandle, "WorkerW", NULL);
+  }
+  return true;
+}
+
+HWND GetWorkerW()
+{
+  int result;
+  HWND windowHandle = FindWindow("Progman", NULL);
+  SendMessageTimeout(windowHandle, 0x052c, 0, 0, SMTO_NORMAL, 0x3e8, (PDWORD_PTR)&result);
+  EnumWindows(EnumWindowsProc, NULL);
+  ShowWindow(_workerw, SW_HIDE);
+  return windowHandle;
+}
+
+namespace demo
+{
+
+using v8::Exception;
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
+
+void SetWallPaper(const FunctionCallbackInfo<Value> &args)
+{
+  Isolate *isolate = args.GetIsolate();
+
+  if (args.Length() < 1)
+  {
+    // Throw an Error that is passed back to JavaScript
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
+  }
+
+  unsigned char *bufferData = (unsigned char *)node::Buffer::Data(args[0]->ToObject());
+  unsigned long handle = *reinterpret_cast<unsigned long *>(bufferData);
+  HWND hwnd = (HWND)handle;
+
+  SetParent(hwnd, GetWorkerW());
+
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "done"));
+}
+
+void Initialize(Local<Object> exports)
+{
+  NODE_SET_METHOD(exports, "setWallpaper", SetWallPaper);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+
+} // namespace demo
